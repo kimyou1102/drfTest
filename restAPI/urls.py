d8362e3c0c83a632e0e8bbe72826f7dde969f107
kimyou1102 <yoosk5485@naver.com>
2022-07-16 02:18:23 +0900

    url path수정

--- restAPI/urls.py
+++ restAPI/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('test', views.PostList.as_view()),
+    path('test/', views.PostList.as_view()),
     path('test/<int:pk>/', views.PostDetail.as_view()),
 ]
